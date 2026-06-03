7454fb7701c9906ae6ba68382dfad79956e53681
Mikhail Ussynin <musynin@edu.hse.ru>
2026-06-03 14:13:14 +0300

    Update .gitignore and local scripts

--- scripts/run_query.sh
+++ scripts/run_query.sh
@@ -2,4 +2,8 @@
 
 set -euo pipefail
 
-./build/tests/test_queries --run-query "$1" "$2" > "$3" 2> "$4"
+for Q in {0..42}; do
+    OUTPUT=/home/mike/Columnar-Engine/small/query_${Q}.csv
+    LOGS=/home/mike/Columnar-Engine/small/query_${Q}.log
+    ./build/tests/test_queries --run-query "${Q}" "/home/mike/Columnar-Engine/tests/output.columnar" > "${OUTPUT}"
+done
