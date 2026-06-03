1e30f3bcf06b226d5361254de4a171c59dd5ecce
Mikhail Ussynin <musynin@edu.hse.ru>
2026-06-03 15:34:32 +0300

    Fix scripts and test files

--- scripts/run_query.sh
+++ scripts/run_query.sh
@@ -2,8 +2,9 @@
 
 set -euo pipefail
 
-for Q in {0..42}; do
+#for Q in {0..42}; do
+for Q in {31..31}; do
     OUTPUT=/home/mike/Columnar-Engine/small/query_${Q}.csv
     LOGS=/home/mike/Columnar-Engine/small/query_${Q}.log
-    ./build/tests/test_queries --run-query "${Q}" "/home/mike/Columnar-Engine/tests/output.columnar" > "${OUTPUT}"
+    ./build/tests/test_queries --run-query "${Q}" tests/big.columnar
 done
